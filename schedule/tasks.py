e9284a70f4ba0c53e94ab5cff02b28c13a5ec762
jindalpawan <jindalpawan99@gmail.com>
2020-06-11 22:41:16 +0530

    done

--- schedule/tasks.py
+++ schedule/tasks.py
@@ -1,13 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 from datetime import datetime, timedelta
 from celery import Celery
-from celery import shared_task,current_task, task
 from hero.celery import app
 
 
-temp=0
-
 @app.task
-def hello():
-	temp=1
+def test_sample():
+	print("Done")
 	return 1
--- schedule/views.py
+++ schedule/views.py
@@ -1,33 +1,20 @@
 import os
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from django.core.exceptions import ImproperlyConfigured
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework import viewsets
 import json
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, DetailView
 from django.utils import timezone
 from datetime import datetime, timedelta
-from .tasks import hello, temp
+from .tasks import test_sample
 
-'''
-class Home(TemplateView):
-	def get(self, request):
-		print((datetime.now()+timedelta(minutes=1)))
-		hello.apply_async(eta=(datetime.now()+timedelta(minutes=1)))
-		return HttpResponse()
-'''
 
 class Home(APIView):
 	def get(self, request):
 		data=json.dumps(request.data)
 		data=json.loads(data)
-		hello.apply_async(eta=data["datetime"])
-		if temp:
-			dic={'response':"Success"}
-		else:
-			dic={'response':"Failed"}
-		return Response(dic)
+		r=test_sample.apply_async(eta=data["datetime"])
+		if r:
+			return Response(status=status.HTTP_200_OK)
+		return Response(status=status.HTTP_404_NOT_FOUND)
